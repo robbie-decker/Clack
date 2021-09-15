c310be7331b3712f55a338dc88e0c6a5b43fa898
Robbie Decker <robbd56@gmail.com>
2021-09-15 15:37:58 -0400

    Initilize all classes and files (First Commit)

---
+++ Clack/src/data/MessageClackData.java
@@ -0,0 +1,5 @@
+package data;
+
+public class MessageClackData {
+    
+}
--- Clack/src/test/test.java
+++ Clack/src/test/test.java
@@ -2,8 +2,6 @@ package test;
 
 public class test {
     public static void main(String[] args){
-
         System.out.println("Hello World");
-
     }
 }
