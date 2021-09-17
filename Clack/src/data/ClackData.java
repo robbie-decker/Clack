f3dd32cda633c03cae5f45ccc62a49545a8cfea5
chrishful <89863527+chrishful@users.noreply.github.com>
2021-09-17 10:49:18 -0400

    ClackData structure and variables

--- Clack/src/data/ClackData.java
+++ Clack/src/data/ClackData.java
@@ -1,5 +1,41 @@
 package data;
 
+/**
+ *
+ * @author Chris Hickman and Robbie Decker
+ */
 public class ClackData {
-    
+    private String userName;
+    private int type;
+    private Date date;                           // <-- do we have to implement class Date?
+    private static int CONSTANT_LISTUSERS = 0;
+    private static int CONSTANT_LOGOUT = 1;
+    private static int CONSTANT_SENDMESSAGE = 2; // <-- how we declare constants????
+    private static int CONSTANT_SENDFILE = 3;
+
+
+    public ClackData(String userName, int type){
+        this.userName = userName;
+        this.type = type;
+        this.date = new Date();
+    }
+    public ClackData(int type) {
+        this("anon", type);
+    }
+    public ClackData(){
+        this("anon", CONSTANT_LISTUSERS);// <-- discuss whether this is the best constant to use
+    }
+    public int getType(){
+
+    }
+    public String getUserName(){
+
+    }
+    public Date getDate(){
+
+    }
+    public int getData(){
+
+    }
+
 }
--- Clack/src/test/TestClackServer.java
+++ Clack/src/test/TestClackServer.java
@@ -1,5 +1,9 @@
 package test;
 
 public class TestClackServer {
-    
+    public static void main(String[] args){
+
+        System.out.println("my life");
+
+    }
 }
