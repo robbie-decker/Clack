c55a6c1603ba81ae3780a7afb8a4159c73b3d9db
chrishful <89863527+chrishful@users.noreply.github.com>
2021-09-18 12:13:32 -0400

    Structure for ClackClient

--- Clack/src/main/ClackClient.java
+++ Clack/src/main/ClackClient.java
@@ -1,5 +1,49 @@
 package main;
+import data.ClackData;
 
 public class ClackClient {
+    private String userName;
+    private String hostName;
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToSendToServer;
+    private ClackData dataToReceiveFromServer;
+
+public ClackData(String userName, String hostName, int port ){
+}
+public ClackData(String userName, String hostName) {
+}
+public ClackData(String userName) {
+}
+public ClackData() {
+}
+public void start(){}
+public void readClientData(){}
+public void sendData(){}
+public void receiveData(){}
+public void printData(){}
+public String getUserName(){
+
+}
+public String getHostName(){
+
+}
+public int getPort(){
+
+}
+public int hashCode(){
+
+}
+public ClackClient equals(ClackClient toBeSet){
+
+}
+public String toString(){
     
+}
+
+
+
+
+
+
 }
