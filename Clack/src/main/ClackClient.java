c4aab394f1ee97fbd7f7ec83dc9e5003e88e61e8
chrishful <89863527+chrishful@users.noreply.github.com>
2021-09-24 17:35:32 -0400

    More of the JavaDoc

--- Clack/src/main/ClackClient.java
+++ Clack/src/main/ClackClient.java
@@ -1,6 +1,11 @@
 package main;
 import data.*;
 
+/**
+ * A class that represents a client user.
+ *
+ * @author Chris Hickman
+ */
 public class ClackClient {
     private String userName;
     private String hostName;
@@ -10,8 +15,15 @@ public class ClackClient {
     private ClackData dataToReceiveFromServer;
     public static final int defaultPort = 7000;
 
-
-public ClackClient(String userName, String hostName, int port ){
+    /**
+     * Constructor for ClackClient that takes in a user-defined user name, host name and port number.
+     * Sets closed connection to true, and the data sent/received from and to the server to null.
+     *
+     * @param userName The user name of the object.
+     * @param hostName The name of the computer representing the server.
+     * @param port The port number of the server connected to.
+     */
+    public ClackClient(String userName, String hostName, int port ){
     this.userName = userName;
     this.hostName = hostName;
     this.port = port;
@@ -19,53 +31,121 @@ public ClackClient(String userName, String hostName, int port ){
     this.dataToSendToServer = null;
     this.dataToReceiveFromServer = null;
 }
-public ClackClient(String userName, String hostName) {
+
+    /**
+     * Constructor that takes in a user-defined user name and host name, sets the port number to defaultPort.
+     *
+     * @param userName The user name of the client.
+     * @param hostName The name of the computer representing the server.
+     */
+    public ClackClient(String userName, String hostName) {
     this(userName, hostName, defaultPort);
 }
-public ClackClient(String userName) {
+
+    /**
+     * Constructor that receives a user defined user name.
+     *
+     * @param userName The user name of the client.
+     */
+    public ClackClient(String userName) {
     this(userName, "localhost");
 }
-public ClackClient() {
+
+    /**
+     * Constructor for ClackClient that takes no values.
+     */
+    public ClackClient() {
     this("anon");
 }
-public void start(){}
-public void readClientData(){}
-public void sendData(){}
-public void receiveData(){}
-public void printData(){}
-public String getUserName(){
+
+    /**
+     *
+     */
+    public void start(){}
+
+    /**
+     * Recieves data from the client.
+     */
+    public void readClientData(){}
+
+    /**
+     * Sends data to the server.
+     */
+    public void sendData(){}
+
+    /**
+     * Receives data from the server.
+     */
+    public void receiveData(){}
+
+    /**
+     * Prints the received data to the standard output
+     */
+    public void printData(){}
+
+    /**
+     * Accessor for user name.
+     *
+     * @return The name of the client.
+     */
+    public String getUserName(){
     return this.userName;
 }
-public String getHostName(){
+
+    /**
+     * Accessor for the host name.
+     *
+     * @return Name of the computer representing the server.
+     */
+    public String getHostName(){
     return this.hostName;
 }
-public int getPort(){
+
+    /**
+     * Accessor for the port number.
+     *
+     * @return POrt number on server connected to.
+     */
+    public int getPort(){
     return this.port;
 }
-public int hashCode(){
+
+    /**
+     * Generates a hash code for the object.
+     *
+     * @return a hash code representing the object.
+     */
+    public int hashCode(){
     int result = 13;
     result = 31*result + port;
     result = 31*result + userName.hashCode();
     result = 31*result + hostName.hashCode();
     return result;
-
 }
 
+    /**
+     * Checks to see if two ClackClients are equal.
+     *
+     * @param toBeSet Object being compared to ClackClient.
+     * @return A boolean representing whether the objects are equal to each other.
+     */
+    public boolean equals(Object toBeSet){
 
-public boolean equals(Object toBeSet){
-
-if (toBeSet == null) return false;
-
-    else if(toBeSet instanceof ClackClient) {
-
-        ClackClient dummy = (ClackClient) toBeSet; // <-- needs to be tweaked
-        return this.hashCode() == dummy.hashCode();
-
+        if (toBeSet == null) return false;
+        else if(toBeSet instanceof ClackClient) {
+            ClackClient dummy = (ClackClient) toBeSet; // <-- needs to be tweaked
+            return this.hashCode() == dummy.hashCode();
     }
-    else return false;
+        else return false;
 
 }
-public String toString() {
+
+    /**
+     * Returns a string representing a full description of the class.
+     *
+     * @return A String representing a full description of the class.
+     */
+    public String toString() {
     return "user name: " + this.userName + ", host name: " + this.hostName + ", port: " + this.port +
             ", closed connection: " + String.valueOf(closeConnection);
 }
--- Clack/src/test/TestClackData.java
+++ Clack/src/test/TestClackData.java
@@ -28,8 +28,8 @@ public class TestClackData {
  */
  FileClackData cd1 = new FileClackData("Chris", "this.txt", 1); //constructors work
  FileClackData fcd1 = new FileClackData();
- System.out.println(fcd1.toString()); // toString works
- System.out.println(cd1.toString());
+ System.out.println("fcd1 String: " + fcd1.toString()); // toString works
+ System.out.println("cd1 String: " + cd1.toString());
  System.out.println("fcd1's type: " + fcd1.getType()); //accessor for type works
  System.out.println("cd1's type: " + cd1.getType());
  System.out.println(fcd1.getUserName());
@@ -49,7 +49,7 @@ public class TestClackData {
         System.out.println("cd2 = cd1? " + cd2.equals(cd1));
         System.out.println(cd2.hashCode() + " = " + cd1.hashCode());
         cd2.setFileName("this.txt"); //mutator works
-        
+
         System.out.println("Changed fileName from cd2 to the same as cd1.");
         System.out.println(cd2.hashCode() + " = " + cd1.hashCode());
         System.out.println("cd2 = cd1? " + cd2.equals(cd1));
