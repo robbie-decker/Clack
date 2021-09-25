c9a8900a38a0ac369fc7339f590e46980c01a93f
chrishful <89863527+chrishful@users.noreply.github.com>
2021-09-24 21:49:48 -0400

    Unit testing done

--- Clack/src/test/TestClackData.java
+++ Clack/src/test/TestClackData.java
@@ -20,37 +20,37 @@ public class TestClackData {
         System.out.println("cd1's user name: " + cd1.getUserName());
         System.out.println("cd1's Date: " + cd1.getDate());
         System.out.println("cd1's Data: " + cd1.getData());
-        System.out.println("cd1's file name: " + cd1.getFileName());
-        cd1.setFileName("that.txt");
+        System.out.println("cd1's file name: " + ((FileClackData)cd1).getFileName());
+        ((FileClackData)cd1).setFileName("that.txt");
         System.out.println("Changed the file name.");
         System.out.println("cd1 String: " + cd1.toString());
         System.out.println("cd1's type: " + cd1.getType());
         System.out.println("cd1's user name: " + cd1.getUserName());
         System.out.println("cd1's Date: " + cd1.getDate());
         System.out.println("cd1's Data: " + cd1.getData());
-        System.out.println("cd1's file name: " + cd1.getFileName() + "\n");
+        System.out.println("cd1's file name: " + ((FileClackData)cd1).getFileName() + "\n");
 
-    FileClackData fcd1 = new FileClackData();
+    ClackData fcd1 = new FileClackData();
         System.out.println("fcd1 String: " + fcd1.toString()); // toString works
         System.out.println("fcd1's type: " + fcd1.getType()); //accessor for type works
         System.out.println("fcd1's user name: " + fcd1.getUserName());//accessor for user name works
         System.out.println("fcd1's Date: " + fcd1.getDate()); //accessor for date works
         System.out.println("fcd1's Data: " + fcd1.getData()); //accessor for data works
-        System.out.println("fcd1's file name: " + fcd1.getFileName()); //accessor for file name works
-        fcd1.setFileName("notanon"); //set file name works
+        System.out.println("fcd1's file name: " + ((FileClackData)fcd1).getFileName()); //accessor for file name works
+        ((FileClackData)fcd1).setFileName("notanon"); //set file name works
         System.out.println("Changed the file name.");
         System.out.println("fcd1 String: " + fcd1.toString());
         System.out.println("fcd1's type: " + fcd1.getType());
         System.out.println("fcd1's user name: " + fcd1.getUserName());
         System.out.println("fcd1's Date: " + fcd1.getDate());
         System.out.println("fcd1's Data: " + fcd1.getData());
-        System.out.println("fcd1's file name: " + fcd1.getFileName() + "\n");
+        System.out.println("fcd1's file name: " + ((FileClackData)fcd1).getFileName() + "\n");
 
-        FileClackData cd2 = new FileClackData("Chris", "e.txt", 1);
+        ClackData cd2 = new FileClackData("Chris", "e.txt", 1);
         System.out.println(cd1.hashCode());
         System.out.println(cd2.hashCode());
         System.out.println("cd2 = cd1? " + cd2.equals(cd1));
-        cd2.setFileName("that.txt"); //mutator works
+        ((FileClackData)cd2).setFileName("that.txt"); //mutator works
         System.out.println("Changed fileName from cd2 to the same as cd1.");
         System.out.println(cd2.hashCode() + " = " + cd1.hashCode());
         System.out.println("cd2 = cd1? " + cd2.equals(cd1) + "\n\n\n");
