322a81f4a8b1aaeef02363783f3ad296aefd8e39
Robbie Decker <robbd56@gmail.com>
2021-09-26 15:54:09 -0400

    Initialize methods for files

--- Clack/src/data/MessageClackData.java
+++ Clack/src/data/MessageClackData.java
@@ -1,5 +1,29 @@
 package data;
 
-public class MessageClackData {
+public class MessageClackData extends ClackData {
+    private String message;
+
+    public MessageClackData(String userName, String message, int type){
+
+    }
+
+    public MessageClackData(){
+        this("anon", "anon", 0);
+    }
     
+    public void getData(){
+
+    }
+
+    public int hashCode(){
+        return 0;
+    }
+
+    public boolean equals(Object other){
+        return false;
+    }
+
+    public String toString(){
+        return "hello";
+    }
 }
